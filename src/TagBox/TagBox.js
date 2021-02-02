5a1078dbe0ecdb4a96fe748bf786e3c5bd6f9ff1
Wes Glassmeyer <wes.glassmeyer@gmail.com>
2021-02-01 21:14:38 -0700

    Update TagBox.js

--- src/TagBox/TagBox.js
+++ src/TagBox/TagBox.js
@@ -1,16 +1,23 @@
 import React from "react";
 import "./TagBox.css";
 
-export default function TagBox() {
+export default function TagBox(props) {
   return (
     <div>
-      <input type="checkbox" id="Positions" name="Mount" value="Mount" />
+      <input
+        type="checkbox"
+        id="Positions"
+        name="Mount"
+        value="Mount"
+        onChange={props.onClick}
+      />
       <label htmlFor="Mount"> Mount</label>
       <input
         type="checkbox"
         id="Positions"
         name="Back Mount"
         value="Back Mount"
+        onChange={props.onClick}
       />
       <label htmlFor="Back Mount"> Back Mount</label>
       <input
@@ -18,21 +25,47 @@ export default function TagBox() {
         id="Positions"
         name="Side Control"
         value="Side Control"
+        onChange={props.onClick}
       />
       <label htmlFor="Side Control"> Side Control</label>
-      <input type="checkbox" id="Positions" name="Guard" value="Guard" />
+      <input
+        type="checkbox"
+        id="Positions"
+        name="Guard"
+        value="Guard"
+        onChange={props.onClick}
+      />
       <label htmlFor="Guard"> Guard</label>
-      <input type="checkbox" id="Submissions" name="Choke" value="Choke" />
+      <input
+        type="checkbox"
+        id="Submissions"
+        name="Choke"
+        value="Choke"
+        onChange={props.onClick}
+      />
       <label htmlFor="Choke"> Choke</label>
-      <input type="checkbox" id="Submissions" name="Arm Bar" value="Arm Bar" />
+      <input
+        type="checkbox"
+        id="Submissions"
+        name="Arm Bar"
+        value="Arm Bar"
+        onChange={props.onClick}
+      />
       <label htmlFor="Arm Bar"> Arm Bar</label>
-      <input type="checkbox" id="Submissions" name="Kimura" value="Kimura" />
+      <input
+        type="checkbox"
+        id="Submissions"
+        name="Kimura"
+        value="Kimura"
+        onChange={props.onClick}
+      />
       <label htmlFor="Kimura"> Kimura</label>
       <input
         type="checkbox"
         id="Submissions"
         name="Triangle"
         value="Triangle"
+        onChange={props.onClick}
       />
       <label htmlFor="Triangle"> Triangle</label>
       <input
@@ -40,15 +73,40 @@ export default function TagBox() {
         id="Submissions"
         name="Leg Locks"
         value="Leg Locks"
+        onChange={props.onClick}
       />
       <label htmlFor="Leg Locks"> Leg Locks</label>
-      <input type="checkbox" id="action" name="Escape" value="Escape" />
+      <input
+        type="checkbox"
+        id="action"
+        name="Escape"
+        value="Escape"
+        onChange={props.onClick}
+      />
       <label htmlFor="Escape"> Escape</label>
-      <input type="checkbox" id="action" name="Control" value="Control" />
+      <input
+        type="checkbox"
+        id="action"
+        name="Control"
+        value="Control"
+        onChange={props.onClick}
+      />
       <label htmlFor="Control"> Control</label>
-      <input type="checkbox" id="action" name="Attack" value="Attack" />
+      <input
+        type="checkbox"
+        id="action"
+        name="Attack"
+        value="Attack"
+        onChange={props.onClick}
+      />
       <label htmlFor="Attack"> Attack</label>
-      <input type="checkbox" id="action" name="Reversal" value="Reversal" />
+      <input
+        type="checkbox"
+        id="action"
+        name="Reversal"
+        value="Reversal"
+        onChange={props.onClick}
+      />
       <label htmlFor="Reversal"> Reversal</label>
     </div>
   );
