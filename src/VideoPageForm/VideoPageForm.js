986794088136ad0052d3a1663c566cc34b088070
Wes Glassmeyer <wes.glassmeyer@gmail.com>
2021-02-03 20:03:30 -0700

    Update VideoPageForm.js

--- src/VideoPageForm/VideoPageForm.js
+++ src/VideoPageForm/VideoPageForm.js
@@ -41,7 +41,7 @@ export default class VideoPageForm extends Component {
       <form onSubmit={this.handleSubmit}>
         <p>
           Select a rating and the tags below to add this video to the Community
-          Resource Page
+          Favorites Page
         </p>
         <TagBox onClick={this.handleChangeTag} />
         <Rating
