8db6f464b1c58286fb7e470f1501d1cc9a36c905
Wes Glassmeyer <wes.glassmeyer@gmail.com>
2021-02-04 21:08:54 -0700

    Update FavItem.js

--- src/FavItem/FavItem.js
+++ src/FavItem/FavItem.js
@@ -1,26 +1,23 @@
-import React, { Component } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import dummydata from "../dummydata";
 import Rating from "../Rating/Rating";
 
-export default class FavPage extends Component {
-  render() {
-    return (
-      <div>
-        <img
-          id="img"
-          className="style-scope yt-img-shadow"
-          alt=""
-          width="120"
-          src={dummydata.items[0].snippet.thumbnails.default.url}
-        />
+export default function FavItem(props) {
+  return (
+    <div>
+      <img
+        id="img"
+        className="style-scope yt-img-shadow"
+        alt=""
+        width="120"
+        src={props.favItem && props.favItem.snippet.thumbnails.default.url}
+      />
 
-        <Link to={`/video/${dummydata.items[0].id.videoId}`}>
-          {dummydata.items[0].snippet.title}
-        </Link>
-        <Rating value={dummydata.items[0].rating} />
-        <p>tags:{dummydata.items[0].tags.join()}</p>
-      </div>
-    );
-  }
+      <Link to={`/video/${props.favItem && props.favItem.id.videoId}`}>
+        {props.favItem && props.favItem.snippet.title}
+      </Link>
+      <Rating value={props.favItem && props.favItem.rating} />
+      <p>tags:{props.favItem && props.favItem.tags.join()}</p>
+    </div>
+  );
 }
