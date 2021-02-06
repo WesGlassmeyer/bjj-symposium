c381ffe035e95284ae303b811e01f674620395d7
Wes Glassmeyer <wes.glassmeyer@gmail.com>
2021-02-05 19:48:15 -0700

    Update TagBox.js

--- src/TagBox/TagBox.js
+++ src/TagBox/TagBox.js
@@ -2,112 +2,50 @@ import React from "react";
 import "./TagBox.css";
 
 export default function TagBox(props) {
+  const positions = ["Mount", "Back Mount", "Side Control", "Guard"];
+  const submissions = ["Choke", "Arm Bar", "Kimura", "Triangle", "Leg Locks"];
+  const actions = ["Escape", "Reversal", "Control", "Attack"];
+
   return (
     <div>
-      <input
-        type="checkbox"
-        id="Positions"
-        name="Mount"
-        value="Mount"
-        onChange={props.onClick}
-      />
-      <label htmlFor="Mount"> Mount</label>
-      <input
-        type="checkbox"
-        id="Positions"
-        name="Back Mount"
-        value="Back Mount"
-        onChange={props.onClick}
-      />
-      <label htmlFor="Back Mount"> Back Mount</label>
-      <input
-        type="checkbox"
-        id="Positions"
-        name="Side Control"
-        value="Side Control"
-        onChange={props.onClick}
-      />
-      <label htmlFor="Side Control"> Side Control</label>
-      <input
-        type="checkbox"
-        id="Positions"
-        name="Guard"
-        value="Guard"
-        onChange={props.onClick}
-      />
-      <label htmlFor="Guard"> Guard</label>
-      <input
-        type="checkbox"
-        id="Submissions"
-        name="Choke"
-        value="Choke"
-        onChange={props.onClick}
-      />
-      <label htmlFor="Choke"> Choke</label>
-      <input
-        type="checkbox"
-        id="Submissions"
-        name="Arm Bar"
-        value="Arm Bar"
-        onChange={props.onClick}
-      />
-      <label htmlFor="Arm Bar"> Arm Bar</label>
-      <input
-        type="checkbox"
-        id="Submissions"
-        name="Kimura"
-        value="Kimura"
-        onChange={props.onClick}
-      />
-      <label htmlFor="Kimura"> Kimura</label>
-      <input
-        type="checkbox"
-        id="Submissions"
-        name="Triangle"
-        value="Triangle"
-        onChange={props.onClick}
-      />
-      <label htmlFor="Triangle"> Triangle</label>
-      <input
-        type="checkbox"
-        id="Submissions"
-        name="Leg Locks"
-        value="Leg Locks"
-        onChange={props.onClick}
-      />
-      <label htmlFor="Leg Locks"> Leg Locks</label>
-      <input
-        type="checkbox"
-        id="action"
-        name="Escape"
-        value="Escape"
-        onChange={props.onClick}
-      />
-      <label htmlFor="Escape"> Escape</label>
-      <input
-        type="checkbox"
-        id="action"
-        name="Control"
-        value="Control"
-        onChange={props.onClick}
-      />
-      <label htmlFor="Control"> Control</label>
-      <input
-        type="checkbox"
-        id="action"
-        name="Attack"
-        value="Attack"
-        onChange={props.onClick}
-      />
-      <label htmlFor="Attack"> Attack</label>
-      <input
-        type="checkbox"
-        id="action"
-        name="Reversal"
-        value="Reversal"
-        onChange={props.onClick}
-      />
-      <label htmlFor="Reversal"> Reversal</label>
+      {positions.map((position) => (
+        <>
+          <input
+            type="checkbox"
+            key={position}
+            value={position}
+            onChange={props.onClick}
+            aria-label={position}
+          />
+          <label htmlFor={position}> {position}</label>
+        </>
+      ))}
+
+      {submissions.map((submission) => (
+        <>
+          <input
+            type="checkbox"
+            key={submission}
+            value={submission}
+            onChange={props.onClick}
+            aria-label={submission}
+          />
+          <label htmlFor={submission}> {submission}</label>
+        </>
+      ))}
+
+      {actions.map((action) => (
+        <>
+          <input
+            type="checkbox"
+            key={action}
+            value={action}
+            onChange={props.onClick}
+            aria-label={action}
+          />
+          <label htmlFor={action}> {action}</label>
+        </>
+      ))}
     </div>
   );
 }
