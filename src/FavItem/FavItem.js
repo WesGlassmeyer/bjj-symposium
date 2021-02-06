a79978925c94c9d0d99d281812def90546d0fec5
Wes Glassmeyer <wes.glassmeyer@gmail.com>
2021-02-06 12:32:12 -0700

    Update FavItem.js

--- src/FavItem/FavItem.js
+++ src/FavItem/FavItem.js
@@ -8,15 +8,16 @@ export default function FavItem(props) {
       <img
         className="style-scope yt-img-shadow"
         alt=""
-        width="120"
+        width="90"
         src={props.favItem && props.favItem.snippet.thumbnails.default.url}
       />
-
-      <Link to={`/video/${props.favItem && props.favItem.id.videoId}`}>
-        {props.favItem && props.favItem.snippet.title}
-      </Link>
-      <Rating value={props.favItem && props.favItem.rating} />
-      <p>tags:{props.favItem && props.favItem.tags.join()}</p>
+      <div className="text-flex">
+        <Link to={`/video/${props.favItem && props.favItem.id.videoId}`}>
+          {props.favItem && props.favItem.snippet.title}
+        </Link>
+        <Rating value={props.favItem && props.favItem.rating} />
+        <p>tags:{props.favItem && props.favItem.tags.join()}</p>
+      </div>
     </li>
   );
 }
