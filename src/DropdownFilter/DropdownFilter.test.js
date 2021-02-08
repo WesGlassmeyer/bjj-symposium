9b4d3f543d9dd9f8047868fdd64c60aa66d2d148
Wes Glassmeyer <wes.glassmeyer@gmail.com>
2021-02-07 19:57:14 -0700

    Update DropdownFilter.test.js

--- src/DropdownFilter/DropdownFilter.test.js
+++ src/DropdownFilter/DropdownFilter.test.js
@@ -1,9 +1,23 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import DropdownFilter from "./DropdownFilter";
+import React from "react";
+import ReactDOM from "react-dom";
+import DropdownFilter from "./DropdownFilter";
 
-// it("renders without crashing", () => {
-//   const div = document.createElement("div");
-//   ReactDOM.render(<DropdownFilter />, div);
-//   ReactDOM.unmountComponentAtNode(div);
-// });
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  const dropdownValues = {
+    positions: {
+      id: "positions",
+      label: "Choose a Position",
+      value: ["Mount", "Back Mount", "Side Control", "Guard"],
+    },
+  };
+  ReactDOM.render(
+    <DropdownFilter
+      id={dropdownValues.positions.id}
+      label={dropdownValues.positions.label}
+      value={dropdownValues.positions.value}
+    />,
+    div
+  );
+  ReactDOM.unmountComponentAtNode(div);
+});
