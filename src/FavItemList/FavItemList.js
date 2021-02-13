25f66f8f892113d74b27a314d22d0ef292f92330
Wes Glassmeyer <wes.glassmeyer@gmail.com>
2021-02-13 13:03:10 -0700

    Update FavItemList.js

--- src/FavItemList/FavItemList.js
+++ src/FavItemList/FavItemList.js
@@ -1,16 +1,45 @@
 import React, { Component } from "react";
 import FavItem from "../FavItem/FavItem";
-import dummydata from "../dummydata";
+// import dummydata from "../dummydata";
+import config from "../config";
 
 class FavItemList extends Component {
+  state = {
+    videos: [],
+    error: null,
+  };
+  setVideos = (videos) => {
+    this.setState({
+      videos,
+      error: null,
+    });
+  };
+
+  componentDidMount() {
+    fetch(config.SERVER_endpoint)
+      .then((res) => {
+        if (!res.ok) {
+          return res.json().then((error) => Promise.reject(error));
+        }
+        console.log(res);
+        return res.json();
+      })
+      .then((res) => this.setVideos(res))
+      .catch((error) => {
+        console.error(error);
+        this.setState({ error });
+      });
+  }
+
   render() {
-    const favItems = dummydata.items;
+    const favItems = this.state.videos;
+    console.log(this.state.videos);
 
     return (
       <section className="FavItemList">
         <ul className="FavItemList__list" aria-live="polite">
           {favItems.map((favItem) => (
-            <FavItem key={favItem.id.videoId} favItem={favItem} />
+            <FavItem key={favItem.id} favItem={favItem} />
           ))}
         </ul>
       </section>
