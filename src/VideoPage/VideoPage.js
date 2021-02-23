dfa550ca05d9e3d506fab839e79b9bc661e4471d
Wes Glassmeyer <wes.glassmeyer@gmail.com>
2021-02-23 16:28:30 -0700

    Update VideoPage.js

--- src/VideoPage/VideoPage.js
+++ src/VideoPage/VideoPage.js
@@ -25,7 +25,6 @@ export default class VideoPage extends Component {
     const { videos } = this.context;
     const { videoId } = this.props.match.params;
     const video = videos.find((video) => {
-      console.log(video);
       return video.id.videoId === videoId;
     });
     if (!video) {
