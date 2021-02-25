3e8ace0e2dd83010a3405f553511ca444237d038
Wes Glassmeyer <wes.glassmeyer@gmail.com>
2021-02-24 17:36:04 -0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 const config = {
   API_ENDPOINT: "https://www.googleapis.com/youtube/v3/search",
   API_KEY: process.env.REACT_APP_API_KEY,
-  SERVER_endpoint: "http://localhost:8000/fav_items",
+  SERVER_endpoint:
+    process.env.REACT_APP_SERVER_ENDPOINT || "http://localhost:8000/fav_items",
 };
 
 export default config;
