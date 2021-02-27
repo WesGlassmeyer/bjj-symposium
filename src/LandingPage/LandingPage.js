3a3a27befa6d79baa7b8e40b8cdfbc1f2f6b7c43
Wes Glassmeyer <wes.glassmeyer@gmail.com>
2021-02-27 14:03:12 -0700

    Update LandingPage.js

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -3,24 +3,35 @@ import { Link } from "react-router-dom";
 import "./LandingPage.css";
 
 export default function LandingPage() {
+  const welcomeFont = {
+    fontSize: "28px",
+  };
   return (
     <div>
-      <p>
-        Welcome to BJJ Symposium! Our app allows you to search for Brazilian Jiu
-        Jitsu videos from YouTube. If you find a video you think is valuable,
-        you can rate it, apply tags and commit it to the Community Favorites
-        Page.{" "}
-      </p>
-      <p>
-        There are so many BJJ videos out there. We thought it would be useful to
-        create a place where you can look for videos that have been approved by
-        others in the community and help simplify your search.{" "}
-      </p>
-      <p>
-        Click <Link to="/search">here</Link> to search YouTube or click{" "}
-        <Link to="/community_favorites">here</Link> to check out the Community
-        Favorites.
-      </p>
+      <div className="welcome">
+        <p>
+          <span style={welcomeFont} id="title">
+            Welcome!
+          </span>{" "}
+          <br></br> {"  "}Our app allows you to search for Brazilian Jiu Jitsu
+          videos from YouTube. If you find a video you think is valuable, you
+          can rate it, apply tags and commit it to the Community Favorites Page.{" "}
+          <br></br>
+          <br></br>There are so many BJJ videos out there. We thought it would
+          be useful to create a place where you can look for videos that have
+          been approved by others in the community and help simplify your
+          search.{" "}
+        </p>
+        <div className="landing-page-button container">
+          <button className="button1">
+            <Link to="/search">Search</Link>
+          </button>
+          <br></br>
+          <button className="button1">
+            <Link to="/community_favorites">Community Favorites</Link>
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
