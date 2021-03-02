66eb06fea0b5f29f65a4b48a8447977faad40bd2
Wes Glassmeyer <wes.glassmeyer@gmail.com>
2021-03-01 19:43:16 -0700

    Create MenuItems.js

---
+++ src/Navbar/MenuItems.js
@@ -0,0 +1,17 @@
+export const MenuItems = [
+  {
+    title: "Search",
+    url: "/search",
+    cName: "nav-links",
+  },
+  {
+    title: "Community Favorites",
+    url: "/community_favorites",
+    cName: "nav-links",
+  },
+  {
+    title: "Home",
+    url: "/",
+    cName: "nav-links",
+  },
+];
