dce6c00a6347ed0dd3c8c7cb7602bf18e75d0d5d
Wes Glassmeyer <wes.glassmeyer@gmail.com>
2021-03-01 19:42:09 -0700

    Update FavPage.js

--- src/FavPage/FavPage.js
+++ src/FavPage/FavPage.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import FavItemList from "../FavItemList/FavItemList";
+import "./FavPage.css";
 
 export default class FavPage extends Component {
   render() {
     return (
       <div>
-        <h2>Community Favorites Page</h2>
+        <h2 className="fav-page-title">Community Favorites Page</h2>
 
         <FavItemList />
       </div>
