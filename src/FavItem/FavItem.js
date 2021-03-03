f2dafd44990e2b526b9ea5f43f6f083b89cfe535
Wes Glassmeyer <wes.glassmeyer@gmail.com>
2021-03-03 16:37:55 -0700

    Update FavItem.js

--- src/FavItem/FavItem.js
+++ src/FavItem/FavItem.js
@@ -6,16 +6,22 @@ import "./FavItem.css";
 export default function FavItem(props) {
   return (
     <li className="fav-item">
-      <img
-        className="style-scope yt-img-shadow fav-page-img"
-        alt=""
-        width="90"
-        src={props.favItem && props.favItem.thumbnail}
-      />
+      <Link
+        to={`/video/${props.favItem && props.favItem.youtube_id}`}
+        className="text-flex"
+      >
+        <h3>{props.favItem && props.favItem.title}</h3>
+      </Link>
+      <Link to={`/video/${props.favItem && props.favItem.youtube_id}`}>
+        <img
+          className="style-scope yt-img-shadow fav-page-img"
+          alt={props.favItem.title}
+          width="200"
+          src={props.favItem && props.favItem.thumbnail}
+        />
+      </Link>
+
       <div className="text-flex">
-        <Link to={`/video/${props.favItem && props.favItem.youtube_id}`}>
-          {props.favItem && props.favItem.title}
-        </Link>
         <Rating value={props.favItem && props.favItem.rating} />
         <p className="Fav-page-tags">
           Tags: {props.favItem && props.favItem.tags.join(", ")}
