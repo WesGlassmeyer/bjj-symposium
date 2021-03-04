ff7206adaa41248f7fe90ac4a0ed201e5652f51c
Wes Glassmeyer <wes.glassmeyer@gmail.com>
2021-03-03 20:23:55 -0700

    Add Iframe

---
+++ src/Iframe/Iframe.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Iframe.css";
+
+export default function Iframe(props) {
+  const embedLink = "https://www.youtube.com/embed/" + props.video.id.videoId;
+  const title = props.video.snippet.title;
+  return (
+    <div className="Iframe">
+      <h3 className="Video_title">{props.video.snippet.title}</h3>
+      <div className="Video_link">
+        <iframe
+          className="responsive-iframe"
+          title={title}
+          width="300"
+          height="169"
+          src={embedLink}
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      </div>
+    </div>
+  );
+}
